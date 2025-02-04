2c53eb47f726c6a78833dbc073112b3710afdccc
Fouad Soubra <fouadsoubra@hotmail.com>
2025-02-04 12:34:08 +0200

    Updated MrbsContext and Program.cs to use MySQL instead of SQL Server

--- MeetingRoomBookingSystem/Program.cs
+++ MeetingRoomBookingSystem/Program.cs
@@ -7,38 +7,40 @@ using MRBS.Core.Repositories;
 using MRBS.Services.Interfaces;
 using MRBS.Services;
 using AutoMapper;
-using Swashbuckle.AspNetCore;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
 
-builder.Services.AddCors();// front end error handling
+// Add services to the container.
+builder.Services.AddCors(); // Frontend error handling
 builder.Services.AddControllers();
-//added for controllers
+
+// Added for controllers
 builder.Services.AddScoped<ICompanyService, CompanyService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IRoomService, RoomService>();
 builder.Services.AddScoped<IReservationService, ReservationService>();
 builder.Services.AddAutoMapper(typeof(Program));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Configure Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "MRBS", Version = "v1" });
 });
 
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>(); //This line was added
-var connection = @"Server=(localdb)\mssqllocaldb;Database=MRBS; Trusted_Connection=True; ConnectRetryCount=0";
-builder.Services.AddDbContext<MrbsContext>(options => options.UseSqlServer(connection, b => b.MigrationsAssembly("MeetingRoomBookingSystem")));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>(); // This line was added
+
+// Get the connection string from appsettings.json
+// var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+var connectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
+
+// Use MySQL for EF Core with your connection string
+builder.Services.AddDbContext<MrbsContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), b => b.MigrationsAssembly("MeetingRoomBookingSystem")));
 
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
@@ -46,11 +48,13 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     options.RoutePrefix = string.Empty;
 });
+
 app.UseCors(builder =>
 {
     builder.WithOrigins("http://127.0.0.1:3000") // Replace with your actual front-end origin(s)
@@ -59,9 +63,7 @@ app.UseCors(builder =>
 });
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
